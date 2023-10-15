b7cc519bcfbe49fd85981f91d87b898e397e83c7
tokizuoh <tokizuoh@gmail.com>
2023-10-15 09:39:13 +0900

    add: TokenType

---
+++ Sources/TokenType.swift
@@ -0,0 +1,44 @@
+enum TokenType {
+    case leftParenthesis
+    case RightParenthesis
+    case leftBrace
+    case rightBrace
+    case comma
+    case dot
+    case minus
+    case plus
+    case semicolon
+    case slash
+    case star
+
+    case bang
+    case bangEqual
+    case equalEqual
+    case greater
+    case greaterEqual
+    case less
+    case lessEqual
+
+    case identifier
+    case string
+    case number
+
+    case and
+    case `class`
+    case `else`
+    case `false`
+    case fun
+    case `for`
+    case `if`
+    case `nil`
+    case or
+    case print
+    case `return`
+    case `super`
+    case this
+    case `true`
+    case `var`
+    case `while`
+
+    case eof
+}
